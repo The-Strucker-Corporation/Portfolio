489cabffb2da6a8520618343905a3d645ea853b4
Lennox Matwere <aberamacarleton@gmail.com>
2024-07-07 18:57:57 +0300

    Initial commit

---
+++ app/config.ts
@@ -0,0 +1,26 @@
+import { Metadata } from '@/types/metadata';
+
+const metadata: Metadata = {
+  author: {
+    name: 'Lennox Matwere',
+    username: 'theyardmic',
+    label: 'Developer and Engineer'
+  },
+  site: {
+    title: 'Lennox Matwere - Portfolio Website',
+    description: 'Portfolio of web & UI designer/developer Lennox Matwere.',
+    ogImage: '/images/og.png',
+    url: 'https://www.example.com',
+    keywords: [
+      'web design',
+      'UI design',
+      'web development',
+      'portfolio',
+      'Lennox Matwere'
+    ],
+    language: 'en',
+    charset: 'UTF-8'
+  }
+};
+
+export { metadata };
