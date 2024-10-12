6fe3f998181253439775955b720c761c8eb39590
Lennox Matwere <aberamacarleton@gmail.com>
2024-10-12 20:55:32 +0000

    feat: customizing the metadata to match Nerville's Bio.

--- app/config.ts
+++ app/config.ts
@@ -3,8 +3,8 @@ import { Metadata } from '@/types/metadata';
 const metadata: Metadata = {
   author: {
     name: 'Nerville Ghali',
-    username: 'theyardmic',
-    label: 'Developer and Engineer'
+    username: 'neville-phillippe',
+    label: 'Businessman and IT Specialist.'
   },
   site: {
     title: 'Nerville Ghali - Portfolio Website',
